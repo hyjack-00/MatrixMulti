eb342723cf486847b1036a966bdf127d829e267b
Jack Huang <1197236169@qq.com>
2022-12-05 15:29:31 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
+if [! -d "./build/"]; then
+    mkdir ./build
+    echo "Create ./build/"
+fi
+if [! -d "./bin/"]; then
+    mkdir ./bin
+    echo "Create ./bin/"
+fi
+
 cd ./build/
 cmake ..
 build

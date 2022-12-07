5a9f9346c94baaeab2b8b8d9bb3aeb79320ac334
hyjack <1197236169@qq.com>
2022-12-07 22:34:53 +0800

    modif run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,21 @@
-if [! -d "./build/"]; then
+#!/bin/bash
+
+if [ -d ./build/ ]; then
+    echo "./build/ exist"
+else
     mkdir ./build
     echo "Create ./build/"
 fi
-if [! -d "./bin/"]; then
+
+if [ -d ./bin/ ]; then
+    echo "./bin/ exist"
+else
     mkdir ./bin
     echo "Create ./bin/"
 fi
 
 cd ./build/
 cmake ..
-build
+make
 cd ..
 ./bin/MatrixMulti

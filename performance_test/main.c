9e8891a3f28c99b2dcd21f0f043d0d0e326e07e4
Jack Huang <1197236169@qq.com>
2022-12-12 21:37:42 +0800

    update

--- performance_test/main.c
+++ performance_test/main.c
@@ -83,24 +83,24 @@ int main() {
     printf("NEON test:\n");
     
     printf("fp32-kernel_1 paral\n");
-    test_parallel(kernel_1, threads_num, LOOP, OP_PER_LOOP_K1, 1);
+    test_parallel(kernel_1, 1, threads_num, LOOP, OP_PER_LOOP_K1);
     printf("fp32-kernel_1 single\n\n");
-    test_single(kernel_1, LOOP, OP_PER_LOOP_K1, 1);
+    test_single(kernel_1, 1, LOOP, OP_PER_LOOP_K1);
 
     printf("fp32-kernel_2 paral\n");
-    test_parallel(kernel_2, threads_num, LOOP, OP_PER_LOOP_K2, 1);
+    test_parallel(kernel_2, 1, threads_num, LOOP, OP_PER_LOOP_K2);
     printf("fp32-kernel_2 single\n\n");
-    test_single(kernel_2, LOOP, OP_PER_LOOP_K2, 1);
+    test_single(kernel_2, 1, LOOP, OP_PER_LOOP_K2);
 
     printf("fp64-kernel_3 paral\n");
-    test_parallel(kernel_3, threads_num, LOOP, OP_PER_LOOP_K3, 1);
+    test_parallel(kernel_3, 1, threads_num, LOOP, OP_PER_LOOP_K3);
     printf("fp64-kernel_3 single\n");
-    test_single(kernel_3, LOOP, OP_PER_LOOP_K3, 1);
+    test_single(kernel_3, 1, LOOP, OP_PER_LOOP_K3);
 
     printf("int32-kernel_4 paral\n");
-    test_parallel(kernel_4, threads_num, LOOP, OP_PER_LOOP_K4, 0);
+    test_parallel(kernel_4, 0, threads_num, LOOP, OP_PER_LOOP_K4);
     printf("int32-kernel_4 single\n\n");
-    test_single(kernel_4, LOOP, OP_PER_LOOP_K4, 0);
+    test_single(kernel_4, 0, LOOP, OP_PER_LOOP_K4);
 
     return 0;
 }

26643f9011f047e1f6cf1281f937eb107dfadbd0
Jack Huang <1197236169@qq.com>
2022-12-12 17:51:37 +0800

    update

--- performance_test/FLOPS/build.sh
+++ performance_test/FLOPS/build.sh
@@ -1,4 +1,4 @@
-gcc -fopenmp -c neon_cal.s -o neon_cal.o
-gcc -fopenmp -c main.c     -o main.o
+gcc -fopenmp -D__NEON__ -c neon_cal.s -o neon_cal.o
+gcc -fopenmp -D__NEON__ -c main.c     -o main.o
 
 gcc -fopenmp neon_cal.o main.o -o main

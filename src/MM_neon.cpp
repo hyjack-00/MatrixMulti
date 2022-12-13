cd23c97d07c29bd6494292e4c3e57915996837b0
Jack Huang <1197236169@qq.com>
2022-12-13 21:45:09 +0800

    wrong neonBlocking

--- src/MM_neon.cpp
+++ src/MM_neon.cpp
@@ -234,20 +234,20 @@ void mm_1G_f32_vec_tile(float32_t *A, float32_t *B, float32_t *C,
     float32x4_t C3;
 
     for (int ii = 0; ii < m; ii += Ti) {
-     int iend = MIN(m ,ii+Ti);
-     for (int jj = 0; jj < n; jj += Tj) {
-      int jend = MIN(n, jj+Tj);
-      for (int kk = 0; kk < p; kk += Tk) {
-        int kend = MIN(p, kk+Tk);
+        int iend = MIN(m ,ii+Ti);
+        for (int jj = 0; jj < n; jj += Tj) {
+            int jend = MIN(n, jj+Tj);
+            for (int kk = 0; kk < p; kk += Tk) {
+                int kend = MIN(p, kk+Tk);
 
-        for (int i = 0; i < iend; i += 4) {
-            for (int j = 0; j < jend; j += 4) {
+        for (int i = ii; i < iend; i += 4) {
+            for (int j = jj; j < jend; j += 4) {
                 C0 = vmovq_n_f32(0);
                 C1 = vmovq_n_f32(0);
                 C2 = vmovq_n_f32(0);
                 C3 = vmovq_n_f32(0);
 
-                for (int k = 0; k < kend; k += 4) {
+                for (int k = kk; k < kend; k += 4) {
                     a = i*p + k;
                     b = k*n + j;
 

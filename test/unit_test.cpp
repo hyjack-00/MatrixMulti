1ff5019ff1f8f41b1e17f32a1e9a7985e61732f1
Jack Huang <1197236169@qq.com>
2022-12-13 18:20:08 +0800

    update

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -98,7 +98,7 @@ void test_neon_f32_tile() {
 }
 
 void test_neon_f32() {
-    constexpr int loop = 100, m = 1024, p = 512, n = 512;
+    constexpr int loop = 100, m = 1024, p = 1024, n = 1024;
     OS << "Neon test fp32: Loop-" << loop;
     OS << ", M-" << m << ", P-" << p << ", N-" << n << endl;
     Mat_1G<float> A(m, p), B(p, n), C(m, n), Ans(m, n);

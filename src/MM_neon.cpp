34d56c64f15e7191aeeccdcb3d58fc1049f211a9
Jack Huang <1197236169@qq.com>
2022-12-13 21:48:28 +0800

    update

--- src/MM_neon.cpp
+++ src/MM_neon.cpp
@@ -1,8 +1,8 @@
 #include "MM_neon.h"
 
 #define MIN(x,y) (((x)<(y))?(x):(y))
-// using std::cout;
-// using std::endl;
+using std::cout;
+using std::endl;
 
 #ifdef __ARM_NEON
 
@@ -250,6 +250,7 @@ void mm_1G_f32_vec_tile(float32_t *A, float32_t *B, float32_t *C,
                 for (int k = kk; k < kend; k += 4) {
                     a = i*p + k;
                     b = k*n + j;
+                    cout << "    (" << a << "," b << ")" << endl;
 
                     B0 = vld1q_f32(B + b);
                     B1 = vld1q_f32(B + b + n);
@@ -282,6 +283,7 @@ void mm_1G_f32_vec_tile(float32_t *A, float32_t *B, float32_t *C,
                 }  // k
 
                 c = i*n + j;
+                cout << "C(" << i << "," << j << ")" << endl;
                 vst1q_f32(C + c, C0);
                 vst1q_f32(C + c + n, C1);
                 vst1q_f32(C + c + n*2, C2);

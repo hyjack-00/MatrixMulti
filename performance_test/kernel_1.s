71b1d42462e43283e3e79bdc0ecf599b284060f2
Jack Huang <1197236169@qq.com>
2022-12-13 17:41:25 +0800

    update

--- performance_test/kernel_1.s
+++ performance_test/kernel_1.s
@@ -11,7 +11,7 @@ kernel_1:
     fmla v2.4s, v2.4s, v2.4s
     fmla v3.4s, v3.4s, v3.4s
 
-    ;fmla v4.4s, v4.4s, v4.4s
+    fmla v4.4s, v4.4s, v4.4s
     ;fmla v5.4s, v5.4s, v5.4s
     ;fmla v6.4s, v6.4s, v6.4s
     ;fmla v7.4s, v7.4s, v7.4s

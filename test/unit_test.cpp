30d59974cd767dec83700723274cedd7e11fd6e3
Jack Huang <1197236169@qq.com>
2022-12-13 21:08:38 +0800

    update

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -120,9 +120,9 @@ void test_neon_f32() {
     show_mat_1G(B);
 
     mm_1G_benchmark(A.data, B.data, Ans.data, m, p, n);
-    // show_mat_1G(Ans);
+    show_mat_1G(Ans);
     mm_1G_f32_vec(A.data, B.data, C.data, m, p, n); //, m, n, p);
-    // show_mat_1G(C);
+    show_mat_1G(C);
 
     if (C == Ans) OS << "Correct" << endl;
     else          OS << "Wrong!!" << endl;

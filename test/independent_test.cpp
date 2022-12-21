57f83091eb4354a7674ef10afc35287ef845b072
Jack Huang <1197236169@qq.com>
2022-12-21 16:29:36 +0800

    update

--- test/independent_test.cpp
+++ test/independent_test.cpp
@@ -104,72 +104,72 @@ void mm_neon_f32(float32_t *A, float32_t *B, float32_t *C, int m, int p, int n) 
     } 
 }
 
-void mm_neon_f32_ikj(float32_t *A, float32_t *B, float32_t *C, int m, int p, int n) {
-    int32_t a;
-    int32_t b;
-    int32_t c;
-    float32x4_t A0;
-    float32x4_t A1;
-    float32x4_t A2;
-    float32x4_t A3;
-    float32x4_t B0;
-    float32x4_t B1;
-    float32x4_t B2;
-    float32x4_t B3;
-    float32x4_t C0;
-    float32x4_t C1;
-    float32x4_t C2;
-    float32x4_t C3;
+// void mm_neon_f32_ikj(float32_t *A, float32_t *B, float32_t *C, int m, int p, int n) {
+//     int32_t a;
+//     int32_t b;
+//     int32_t c;
+//     float32x4_t A0;
+//     float32x4_t A1;
+//     float32x4_t A2;
+//     float32x4_t A3;
+//     float32x4_t B0;
+//     float32x4_t B1;
+//     float32x4_t B2;
+//     float32x4_t B3;
+//     float32x4_t C0;
+//     float32x4_t C1;
+//     float32x4_t C2;
+//     float32x4_t C3;
 
-    for (int i = 0; i < m; i += 4) {
-        for (int k = 0; k < p; k += 4) {
-            a = i*p + k;
-            A0 = vld1q_f32(A + a);
-            A1 = vld1q_f32(A + a + p);
-            A2 = vld1q_f32(A + a + p*2);
-            A3 = vld1q_f32(A + a + p*3);
+//     for (int i = 0; i < m; i += 4) {
+//         for (int k = 0; k < p; k += 4) {
+//             a = i*p + k;
+//             A0 = vld1q_f32(A + a);
+//             A1 = vld1q_f32(A + a + p);
+//             A2 = vld1q_f32(A + a + p*2);
+//             A3 = vld1q_f32(A + a + p*3);
 
-            for (int j = 0; j < n; j += 4) {
-                b = k*n + j;
-                c = i*n + j;
-                C0 = vld1q_s32(C + c);
-                C1 = vld1q_s32(C + c + n);
-                C2 = vld1q_s32(C + c + n*2);
-                C3 = vld1q_s32(C + c + n*3);
+//             for (int j = 0; j < n; j += 4) {
+//                 b = k*n + j;
+//                 c = i*n + j;
+//                 C0 = vld1q_f32(C + c);
+//                 C1 = vld1q_f32(C + c + n);
+//                 C2 = vld1q_f32(C + c + n*2);
+//                 C3 = vld1q_f32(C + c + n*3);
 
-                B0 = vld1q_f32(B + b);
-				B1 = vld1q_f32(B + b + n);
-				B2 = vld1q_f32(B + b + n*2);
-				B3 = vld1q_f32(B + b + n*3);
+//                 B0 = vld1q_f32(B + b);
+// 				B1 = vld1q_f32(B + b + n);
+// 				B2 = vld1q_f32(B + b + n*2);
+// 				B3 = vld1q_f32(B + b + n*3);
                 
-                C0 = vfmaq_laneq_f32(C0, B0, A0, 0);
-				C0 = vfmaq_laneq_f32(C0, B1, A0, 1);
-				C0 = vfmaq_laneq_f32(C0, B2, A0, 2);
-				C0 = vfmaq_laneq_f32(C0, B3, A0, 3);
+//                 C0 = vfmaq_laneq_f32(C0, B0, A0, 0);
+// 				C0 = vfmaq_laneq_f32(C0, B1, A0, 1);
+// 				C0 = vfmaq_laneq_f32(C0, B2, A0, 2);
+// 				C0 = vfmaq_laneq_f32(C0, B3, A0, 3);
 
-				C1 = vfmaq_laneq_f32(C1, B0, A1, 0);
-				C1 = vfmaq_laneq_f32(C1, B1, A1, 1);
-				C1 = vfmaq_laneq_f32(C1, B2, A1, 2);
-				C1 = vfmaq_laneq_f32(C1, B3, A1, 3);
+// 				C1 = vfmaq_laneq_f32(C1, B0, A1, 0);
+// 				C1 = vfmaq_laneq_f32(C1, B1, A1, 1);
+// 				C1 = vfmaq_laneq_f32(C1, B2, A1, 2);
+// 				C1 = vfmaq_laneq_f32(C1, B3, A1, 3);
 
-				C2 = vfmaq_laneq_f32(C2, B0, A2, 0);
-				C2 = vfmaq_laneq_f32(C2, B1, A2, 1);
-				C2 = vfmaq_laneq_f32(C2, B2, A2, 2);
-				C2 = vfmaq_laneq_f32(C2, B3, A2, 3);
+// 				C2 = vfmaq_laneq_f32(C2, B0, A2, 0);
+// 				C2 = vfmaq_laneq_f32(C2, B1, A2, 1);
+// 				C2 = vfmaq_laneq_f32(C2, B2, A2, 2);
+// 				C2 = vfmaq_laneq_f32(C2, B3, A2, 3);
 				
-				C3 = vfmaq_laneq_f32(C3, B0, A3, 0);
-				C3 = vfmaq_laneq_f32(C3, B1, A3, 1);
-				C3 = vfmaq_laneq_f32(C3, B2, A3, 2);
-                C3 = vfmaq_laneq_f32(C3, B3, A3, 3);
+// 				C3 = vfmaq_laneq_f32(C3, B0, A3, 0);
+// 				C3 = vfmaq_laneq_f32(C3, B1, A3, 1);
+// 				C3 = vfmaq_laneq_f32(C3, B2, A3, 2);
+//                 C3 = vfmaq_laneq_f32(C3, B3, A3, 3);
 
-                vst1q_f32(C + c, C0);
-                vst1q_f32(C + c + n, C1);
-                vst1q_f32(C + c + n*2, C2);
-                vst1q_f32(C + c + n*3, C3);
-            }
-        }
-    } 
-}
+//                 vst1q_f32(C + c, C0);
+//                 vst1q_f32(C + c + n, C1);
+//                 vst1q_f32(C + c + n*2, C2);
+//                 vst1q_f32(C + c + n*3, C3);
+//             }
+//         }
+//     } 
+// }
 
 int main() {
     int loop = 20, size = 1024;

52094a32f39fc50442e358d70ac3ae2037b2eccf
Jack Huang <1197236169@qq.com>
2022-12-21 16:58:44 +0800

    update

--- test/independent_test_s32.cpp
+++ test/independent_test_s32.cpp
@@ -80,33 +80,33 @@ void * mm_kernel(void * arg_p) {
                 b = k*n + j;
 
                 B0 = vld1q_s32(B + b);
-				B1 = vld1q_s32(B + b + n);
-				B2 = vld1q_s32(B + b + n*2);
-				B3 = vld1q_s32(B + b + n*3);
+                B1 = vld1q_s32(B + b + n);
+                B2 = vld1q_s32(B + b + n*2);
+                B3 = vld1q_s32(B + b + n*3);
 
                 A0 = vld1q_s32(A + a);
                 C0 = vmlaq_laneq_s32(C0, B0, A0, 0);
-				C0 = vmlaq_laneq_s32(C0, B1, A0, 1);
-				C0 = vmlaq_laneq_s32(C0, B2, A0, 2);
-				C0 = vmlaq_laneq_s32(C0, B3, A0, 3);
+                C0 = vmlaq_laneq_s32(C0, B1, A0, 1);
+                C0 = vmlaq_laneq_s32(C0, B2, A0, 2);
+                C0 = vmlaq_laneq_s32(C0, B3, A0, 3);
 
-				A1 = vld1q_s32(A + a + p);
-				C1 = vmlaq_laneq_s32(C1, B0, A1, 0);
-				C1 = vmlaq_laneq_s32(C1, B1, A1, 1);
-				C1 = vmlaq_laneq_s32(C1, B2, A1, 2);
-				C1 = vmlaq_laneq_s32(C1, B3, A1, 3);
+                A1 = vld1q_s32(A + a + p);
+                C1 = vmlaq_laneq_s32(C1, B0, A1, 0);
+                C1 = vmlaq_laneq_s32(C1, B1, A1, 1);
+                C1 = vmlaq_laneq_s32(C1, B2, A1, 2);
+                C1 = vmlaq_laneq_s32(C1, B3, A1, 3);
 
-				A2 = vld1q_s32(A + a + p*2);
-				C2 = vmlaq_laneq_s32(C2, B0, A2, 0);
-				C2 = vmlaq_laneq_s32(C2, B1, A2, 1);
-				C2 = vmlaq_laneq_s32(C2, B2, A2, 2);
-				C2 = vmlaq_laneq_s32(C2, B3, A2, 3);
+                A2 = vld1q_s32(A + a + p*2);
+                C2 = vmlaq_laneq_s32(C2, B0, A2, 0);
+                C2 = vmlaq_laneq_s32(C2, B1, A2, 1);
+                C2 = vmlaq_laneq_s32(C2, B2, A2, 2);
+                C2 = vmlaq_laneq_s32(C2, B3, A2, 3);
 
-				A3 = vld1q_s32(A + a + p*3);
-				C3 = vmlaq_laneq_s32(C3, B0, A3, 0);
-				C3 = vmlaq_laneq_s32(C3, B1, A3, 1);
-				C3 = vmlaq_laneq_s32(C3, B2, A3, 2);
-				C3 = vmlaq_laneq_s32(C3, B3, A3, 3);
+                A3 = vld1q_s32(A + a + p*3);
+                C3 = vmlaq_laneq_s32(C3, B0, A3, 0);
+                C3 = vmlaq_laneq_s32(C3, B1, A3, 1);
+                C3 = vmlaq_laneq_s32(C3, B2, A3, 2);
+                C3 = vmlaq_laneq_s32(C3, B3, A3, 3);
             }
 
             c = i*n + j;

6702404c22ec99fa7d924b6efb7c46418687524d
Jack Huang <1197236169@qq.com>
2022-12-21 17:03:50 +0800

    update

--- test/independent_test_s32.cpp
+++ test/independent_test_s32.cpp
@@ -47,6 +47,62 @@ void mm_benchmark_s32(int *A, int *B, int *C, int m, int p, int n) {
     }
 }
 
+void mm_neon_s32(int *A, int *B, int *C, int m, int p, int n) {
+    int32_t a, b, c;
+    int32x4_t A0, A1, A2, A3;
+    int32x4_t B0, B1, B2, B3;
+    int32x4_t C0, C1, C2, C3;
+
+    for (int i = 0; i < m; i += 4) {
+        for (int j = 0; j < n; j += 4) {
+            C0 = vmovq_n_s32(0);
+            C1 = vmovq_n_s32(0);
+            C2 = vmovq_n_s32(0);
+            C3 = vmovq_n_s32(0);
+
+            for (int k = 0; k < p; k += 4) {
+                a = i*p + k;
+                b = k*n + j;
+
+                B0 = vld1q_s32(B + b);
+                B1 = vld1q_s32(B + b + n);
+                B2 = vld1q_s32(B + b + n*2);
+                B3 = vld1q_s32(B + b + n*3);
+
+                A0 = vld1q_s32(A + a);
+                C0 = vmlaq_laneq_s32(C0, B0, A0, 0);
+                C0 = vmlaq_laneq_s32(C0, B1, A0, 1);
+                C0 = vmlaq_laneq_s32(C0, B2, A0, 2);
+                C0 = vmlaq_laneq_s32(C0, B3, A0, 3);
+
+                A1 = vld1q_s32(A + a + p);
+                C1 = vmlaq_laneq_s32(C1, B0, A1, 0);
+                C1 = vmlaq_laneq_s32(C1, B1, A1, 1);
+                C1 = vmlaq_laneq_s32(C1, B2, A1, 2);
+                C1 = vmlaq_laneq_s32(C1, B3, A1, 3);
+
+                A2 = vld1q_s32(A + a + p*2);
+                C2 = vmlaq_laneq_s32(C2, B0, A2, 0);
+                C2 = vmlaq_laneq_s32(C2, B1, A2, 1);
+                C2 = vmlaq_laneq_s32(C2, B2, A2, 2);
+                C2 = vmlaq_laneq_s32(C2, B3, A2, 3);
+
+                A3 = vld1q_s32(A + a + p*3);
+                C3 = vmlaq_laneq_s32(C3, B0, A3, 0);
+                C3 = vmlaq_laneq_s32(C3, B1, A3, 1);
+                C3 = vmlaq_laneq_s32(C3, B2, A3, 2);
+                C3 = vmlaq_laneq_s32(C3, B3, A3, 3);
+            }
+
+            c = i*n + j;
+            vst1q_s32(C + c, C0);
+            vst1q_s32(C + c + n, C1);
+            vst1q_s32(C + c + n*2, C2);
+            vst1q_s32(C + c + n*3, C3);
+        }
+    } 
+}
+
 struct Arg_s32 {
     int *A, *B, *C;
     int p, n;
@@ -160,8 +216,8 @@ int main() {
 
     start = Now;
     for (int l = 0; l < loop; l ++) {
-        mm_pthrd(A, B, C);
-        // mm_neon_s32(A.data, B.data, C.data, m, p, n);
+        // mm_pthrd(A, B, C);
+        mm_neon_s32(A.data, B.data, C.data, m, p, n);
     }
     end = Now;
     dur = Dur(start, end);
